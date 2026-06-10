b1cb39b463cc9a673c09edd4e8dfe927aacd9f9e
Zinedis <nantenainabakari@gmail.com>
2026-06-10 19:46:24 +0300

    feat: add speaker.service.ts

---
+++ src/modules/speakers/services/speaker.service.ts
@@ -0,0 +1,19 @@
+import { apiFetch } from '@/shared/config/api.config';
+import { Speaker, SpeakerDetailed, SpeakerArraySchema, SpeakerDetailedSchema } from '../types/speaker.types';
+
+export const SpeakerService = {
+
+  getAll: async (): Promise<Speaker[]> => {
+    return apiFetch<Speaker[]>('api/speakers', {
+      schema: SpeakerArraySchema,
+      next: { revalidate: 3600 }
+    });
+  },
+
+  getById: async (id: string): Promise<SpeakerDetailed> => {
+    return apiFetch<SpeakerDetailed>(`api/speakers/${id}`, {
+      schema: SpeakerDetailedSchema,
+      next: { revalidate: 60 }
+    });
+  }
+};
