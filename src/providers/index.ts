7c51d1dab5d179532072f6e31f5f094fc7fe6eb4
DavFilsDev <miharisoadavidfils@gmail.com>
2026-06-11 10:02:22 +0300

    feat(admin): add index for all providers

---
+++ src/providers/index.ts
@@ -0,0 +1,6 @@
+export { authProvider } from "./authProvider";
+export { eventDataProvider } from "./eventDataProvider";
+export { sessionDataProvider } from "./sessionDataProvider";
+export { speakerDataProvider } from "./speakerDataProvider";
+export { roomDataProvider } from "./roomDataProvider";
+export { questionDataProvider } from "./questionDataProvider";
